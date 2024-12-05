e52cee8f12d5ea9a9fc444bfa6f47400b8b98f94
Jerry Qiu <jqiu61@stuy.edu>
2024-12-04 23:02:20 -0500

    miami

--- miami-roleplay/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const contactForm = document.getElementById("contact-form");
-    contactForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        alert("Thank you for your message! We'll get back to you soon.");
-        contactForm.reset();
-    });
-});
